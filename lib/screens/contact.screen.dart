f73adc5d0dcaeaf0c9d27258d560561ccdab0fcc
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-06-09 08:28:46 +0800

    update base

---
+++ lib/screens/contact.screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContactScreen extends StatefulWidget {
+  const ContactScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ContactScreen> createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vimigo Contact"),
+      ),
+    );
+  }
+}
