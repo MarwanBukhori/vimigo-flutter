af020ea4c1cae6eaaabf978ecf5669a60efa3eb3
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-06-09 12:42:16 +0800

    add contacts.json, implement listview builder using loadJsonData

--- lib/screens/contact.screen.dart
+++ lib/screens/contact.screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:vimigo/services/json.services.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({Key? key}) : super(key: key);
@@ -8,14 +9,91 @@ class ContactScreen extends StatefulWidget {
   State<ContactScreen> createState() => _ContactScreenState();
 }
 
-class _ContactScreenState extends State<ContactScreen>{
+class _ContactScreenState extends State<ContactScreen> {
+  final ScrollController _scrollController = ScrollController();
+  final List _displayList = [];
+  List? contacts;
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Vimigo Contact"),
-      ),
+  void initState() {
+    super.initState();
+
+    //retrieve json
+    setting();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Vimigo Contact",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.black,
+            elevation: 0,
+          ),
+          body: contacts == null
+              ? Center(child: CircularProgressIndicator())
+              : Column(children: [
+                  Expanded(
+                    child: ListView.builder(
+                        controller: _scrollController,
+                        itemCount: _displayList.length + 1,
+                        itemBuilder: (context, index) {
+                          if (index < _displayList.length) {
+                            return ListTile(
+                              title: Row(
+                                children: [
+                                  Expanded(
+                                    flex: 9,
+                                    child: Text(
+                                      _displayList[index]['user'],
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                ],
+                              ),
+                              subtitle:
+                              Text(_displayList[index]['phone']),
+                              trailing: IconButton(
+                                onPressed: () {
+                                },
+                                icon: const Icon(Icons.share_outlined),
+                              ),
+                            );
+                          } else {
+                            return Center(
+                              child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 10, bottom: 50),
+                                  child: index >= contacts!.length
+                                      ? const Text(
+                                          "You have reached end of the list")
+                                      : const CircularProgressIndicator()),
+                            );
+                          }
+                        }),
+                  ),
+                ])),
     );
+  }
+
+  setting() async {
+    //load up json data
+    contacts = await JsonServices.loadJsonData("assets/contacts.json");
+
+    _displayList.addAll(contacts!.take(10));
+    setState(() {});
   }
 }
